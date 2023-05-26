2669158e1b9c5d81fe850d5597e0a4d8effc66fe
David Aldurien <david@alduriencomputers.com>
2023-05-26 18:07:11 -0400

    Enabling static file hosting specifically for the admin site. Since this site is designed for low traffic, static hosting is no issue.

--- smb_to_kodi/smb_to_kodi/urls.py
+++ smb_to_kodi/smb_to_kodi/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("tv/", include("tv.urls")),
     path("admin/", admin.site.urls),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
