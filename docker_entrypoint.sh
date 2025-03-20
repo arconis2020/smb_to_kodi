b85e6e027603ac6e625cad97fc2abc4e5b441cfb
David Aldurien <david@alduriencomputers.com>
2025-03-20 16:27:12 -0500

    Adding threads to improve responsiveness.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,7 +3,7 @@ cd $APPDIR
 # First, check for migrations:
 python manage.py migrate --check || python manage.py migrate
 # Now run the server with gunicorn
-gunicorn --bind=0.0.0.0:8001 smb_to_kodi.wsgi &
+gunicorn --bind=0.0.0.0:8001 --threads=10 smb_to_kodi.wsgi &
 while true; do
     python manage.py syncdisk  # Sync all series.
     sleep 900  # Wait 15 minutes between sync runs.
